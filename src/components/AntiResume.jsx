b89506a2eb21023ad137dcca3d47a11b51665046
justinluce <luce@midlandu.edu>
2023-04-24 15:23:25 -0500

    adds intro to resume

--- src/components/AntiResume.jsx
+++ src/components/AntiResume.jsx
@@ -2,6 +2,15 @@ export const AntiResume = () => {
     return (
         <div>
             <h1>Anti-Resume</h1>
+            <p>
+                Welcome to the Anti-Resume. This is a page designed to showcase
+                web design practices that you SHOULDN'T do.
+                If the page annoys you, that means it's working as intended.
+            </p>
+            <p>
+                If you're looking for an actual resume, I would recommend my
+                <a href="www.linkedin.com/in/justin-luce-77260419a">LinkedIn Page</a>.
+            </p>
         </div>
     );
 }
