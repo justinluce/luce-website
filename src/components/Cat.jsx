47139acf243d7379980cc7859bcacc8b8cdf7f4a
justinluce <luce@midlandu.edu>
2023-05-18 16:31:05 -0500

    cat

---
+++ src/components/Cat.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+export const Cat = () => {
+    return (
+        <div style={{marginLeft: '10px', marginRight: '10px', marginTop: '10px', fontSize: '30px'}}>
+            <Typography 
+            variant='h1'
+            textAlign={'center'}
+            >
+            Cat
+            </Typography>
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+                <img height={400} src='images/underConstruction2.png' />
+            </div>
+        </div>
+    );
+}
