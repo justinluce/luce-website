662db064162b106b62f83b1df93cb692af70a9fe
justinluce <luce@midlandu.edu>
2023-06-05 04:56:26 -0500

    fixes max-width error

--- src/shared/components/Menu.jsx
+++ src/shared/components/Menu.jsx
@@ -40,7 +40,7 @@ export const Menu = () => {
       overflowX: 'auto',
       width: '100%',
       justifyContent: 'center',
-      '@media (max-width: 600px)': {
+      '@media (maxWidth: 600px)': {
         justifyContent: 'space-between',
       }
     };
