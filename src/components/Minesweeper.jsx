2b709de70a28c096b5c805b463c20735322a3051
justinluce <luce@midlandu.edu>
2023-07-07 13:37:50 -0500

    adds basic minesweeper board

--- src/components/Minesweeper.jsx
+++ src/components/Minesweeper.jsx
@@ -1,16 +1,109 @@
 import { Typography } from "@mui/material";
-export const Minesweeper = () => {
-    return (
-        <div style={{marginLeft: '10px', marginRight: '10px', marginTop: '10px', fontSize: '30px'}}>
-            <Typography 
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+
+const incrementAdjacentTiles = (board, row, col, size) => {
+    for(let i = Math.max(row - 1, 0); i <= Math.min(row + 1, size - 1); i++) {
+      for(let j = Math.max(col - 1, 0); j <= Math.min(col + 1, size - 1); j++) {
+        if(board[i][j].value !== -1) {
+          board[i][j].value += 1;
+        }
+      }
+    }
+  };
+  
+  const createBoard = (size, mines) => {
+    const board = Array(size).fill(0).map(() => Array(size).fill(null).map(() => ({value: 0, revealed: false, flagged: false})));
+  
+    for(let i = 0; i < mines; i++) {
+      let row, col;
+  
+      // Make sure to place the mine on a tile that is not already a mine
+      do {
+        row = Math.floor(Math.random() * size);
+        col = Math.floor(Math.random() * size);
+      } while(board[row][col].value === -1);
+  
+      board[row][col] = {value: -1, revealed: false};
+  
+      // Call the helper function
+      incrementAdjacentTiles(board, row, col, size);
+    }
+  
+    return board;
+  };
+  
+const Minesweeper = () => {
+  const size = 10;
+  const mines = 10;
+
+  const [board, setBoard] = useState(createBoard(size, mines));
+  const [gameOver, setGameOver] = useState(false);
+
+  const revealSquare = (row, col) => {
+    if (gameOver || board[row][col].revealed || board[row][col].flagged) return;
+
+    let newBoard = JSON.parse(JSON.stringify(board)); 
+    newBoard[row][col].revealed = true;
+
+    //TODO: This does not end the game for some reason
+    if (board[row][col].value === 'X') {
+        setGameOver(true);
+        alert('Game over! You hit a mine.');
+        setBoard(newBoard);
+        return;
+    }
+
+    setBoard(newBoard);
+  };
+
+const toggleFlag = (e, row, col) => {
+    e.preventDefault();
+
+    if (gameOver || board[row][col].revealed) return;
+
+    let newBoard = JSON.parse(JSON.stringify(board)); 
+    newBoard[row][col].flagged = !newBoard[row][col].flagged;
+    setBoard(newBoard);
+  };
+
+const resetBoard = () => {
+    setBoard(createBoard(size, mines));
+    setGameOver(false);
+};
+
+return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <Typography 
             variant='h1'
-            textAlign={'center'}
-            >
+            textAlign={'center'}>
             Minesweeper
-            </Typography>
-            <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-                <img height={400} src='images/underConstruction2.png' />
-            </div>
-        </div>
-    );
-}
+        </Typography>
+        <button onClick={resetBoard} style={{ margin: '20px', height: '40px', width: '160px' }}>Reset</button>
+        <div style={{ margin: 'auto', display: 'block', maxWidth: 'fit-content', border: '2px solid black'}}>
+            {board.map((row, i) => (
+                <div key={i} style={{ fontSize: 0, lineHeight: 0, textAlign: 'center'}}>
+            {row.map((col, j) => (
+                <button 
+                    key={j}
+                    onClick={() => revealSquare(i, j)}
+                    onContextMenu={(e) => toggleFlag(e, i, j)}
+                    style={{ border: 'none', background: 'none', padding: 0, margin: 0, fontSize: '1rem', verticalAlign: 'top' }}
+                    >
+                    {col.revealed ? 
+                        (col.value === -1 ? 
+                        <img draggable="false" src={`${process.env.PUBLIC_URL}/images/minesweeper/TileExploded.png`} style={{height: "40px", width: "40px", display: 'block'}} alt="Mine" /> : 
+                        <img draggable="false" src={`${process.env.PUBLIC_URL}/images/minesweeper/Tile${col.value}.png`} style={{height: "40px", width: "40px", display: 'block'}} alt="Tile" />) : 
+                        col.flagged ? 
+                        <img draggable="false" src={`${process.env.PUBLIC_URL}/images/minesweeper/TileFlag.png`} style={{height: "40px", width: "40px", display: 'block'}} alt="Flag" /> :
+                        <img draggable="false" src={`${process.env.PUBLIC_URL}/images/minesweeper/TileUnknown.png`} style={{height: "40px", width: "40px", display: 'block'}} alt="Empty Tile" />}
+                    </button>
+            ))}
+          </div>
+        ))}
+    </div>
+    </div>
+  );  
+};
+
+export default Minesweeper;
