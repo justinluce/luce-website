e5e5e63ef89b841eb2accd31ec4fb15a0db848d5
justinluce <justinlucedev@gmail.com>
2024-12-29 21:14:25 -0600

    updates fever dream

--- src/components/projects/FeverDreamDetails.jsx
+++ src/components/projects/FeverDreamDetails.jsx
@@ -10,12 +10,12 @@ export const FeverDreamDetails = () => {
                 textAlign={'center'}
                 style={{ marginBottom: '30px' }}
             >
-            <h4>Justin Luce's Fever Dream 1999</h4>
+            <h4>Fever Dream 1999</h4>
             </Typography>
             <p style={{ textAlign: 'center' }}>
                 A video game comprised of a bunch of small mini-games, each one being a parody of a different genre.
                 <br /><br />
-                Currently version 0.2.0.
+                Currently version 0.1.5.
             </p>
             <br />
             <div className='downloadLinks'>
@@ -23,8 +23,8 @@ export const FeverDreamDetails = () => {
                 Direct Download (coming soon)
                 <a href='https://2x4printerpaper.itch.io'>Itch.io</a>
             </div>
-            <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
-                <img height={400} src='images/feverDreamDetails.png' alt='A small, square house with a mountain in the background' title='Fever Dream'/>
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px', marginBottom: '50px'}}>
+                <img height={400} src='images/feverDreamDetails.webp' alt='A small, square house with a mountain in the background' title='Fever Dream'/>
             </div>
         </div>
     );
