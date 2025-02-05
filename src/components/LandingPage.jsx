a5b54106dd7601df9b21041f3b3d3e1810249753
justinluce <justinlucedev@gmail.com>
2025-02-05 15:35:40 -0600

    updates description

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -42,6 +42,7 @@ export const LandingPage = () =>  {
                     <Link className="menu-item" to='/antiresume'>Anti-Resume</Link>
                 </div>
                 <div id='about-paragraph' ref={pRef}>
+                    <h1>Justin Luce</h1>
                     <img src='/images/luceHeadshot.webp' id='headshot' />  
                     <a href="https://github.com/justinluce" id='github-logo'>
                         <img
@@ -51,18 +52,17 @@ export const LandingPage = () =>  {
                         />
                     </a>
                     <p>
-                        Hello! I'm Justin Luce. I am currently an instructor for Midland University's Code Academy 
+                        Hello! I'm Justin Luce. I am currently the director for Midland University's Code Academy 
                         that is focused on full-stack web development. I teach the Re-Entry Program, 
-                        a coding bootcamp designed for incarcerated individuals, 
-                        as well as an online course. I have created multiple video games 
-                        using Unity and Gamemaker Studio. 
+                        a coding bootcamp designed for incarcerated individuals.
+                        I have created multiple video games using Unity and Gamemaker Studio. 
                         I also have extensive experience working with JavaScript/TypeScript, Java, and C#.
                     </p>
                     <br />
                     <p>
                         This is my website, where you can find a collection of things I've made.
                         This ranges from coding projects, to music, to web development 
-                        (including this website!), and video game development. There is also Cat.&nbsp;
+                        (including this website!), and video game development. There is also <i>Cat</i>.&nbsp;
                         <strong>This site is perpetually a work-in-progress</strong>, 
                         as I'll continue creating new things over time.
                     </p>
