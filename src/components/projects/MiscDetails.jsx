73704d9efa2937a68f2d0b7e7516f77292112b04
justinluce <justinlucedev@gmail.com>
2025-04-10 10:04:09 -0500

    re-orders misc projects

--- src/components/projects/MiscDetails.jsx
+++ src/components/projects/MiscDetails.jsx
@@ -5,10 +5,10 @@ export const MiscDetails = () => {
     const [projectOpen, setProjectOpen] = useState(0);
     
     const projectList = [
-        'Minesweeper',
-        'Chess',
         'Tic-Tac-Toe',
-        'Eisenhower Matrix'
+        'Eisenhower Matrix',
+        'Minesweeper',
+        'Chess'
     ]
 
     return (
@@ -21,7 +21,7 @@ export const MiscDetails = () => {
                 <div id='left-container'>
                     {projectList.map((item, index) => (
                         <div 
-                            className={`${projectOpen === index ? 'active' : ''} ${item === 'Minesweeper' ? 'minesweeper' : ''}`} 
+                            className={`${projectOpen === index ? 'active' : ''} ${item === 'Tic-Tac-Toe' ? 'tic-tac-toe' : ''}`} 
                             onClick={() => setProjectOpen(index)}
                         >
                             {item}
@@ -31,25 +31,25 @@ export const MiscDetails = () => {
                 <div id='right-container'>
                     {projectOpen === 0 && (
                         <div>
-                            <h3><a href='https://www.justinlucedev.com/minesweeper'>Minesweeper</a></h3>
-                        </div>                    
-                    )} 
-                    {projectOpen === 1 && (
-                        <div>
-                            <h3><a href='https://www.justinlucedev.com/chess'>Chess</a></h3>
-                        </div>
-                    )} 
-                    {projectOpen === 2 && (
-                        <div>
                             <h3><a href='https://www.justinlucedev.com/tic-tac-toe'>Tic-Tac-Toe</a></h3>
                             <p>An unbeatable Tic-Tac-Toe game.</p>
                             <p>Utilizes the <a href='https://en.wikipedia.org/wiki/Minimax' target='_blank' rel='noopener noreferrer'>Minimax algorithm</a> to always win (or draw) games.</p>
                         </div>
                     )} 
-                    {projectOpen === 3 && (
+                    {projectOpen === 1 && (
                         <div>
                             <h3><a href='https://github.com/justinluce/eisenhower-matrix' target='_blank' rel='noopener noreferrer'>Eisenhower Matrix</a></h3>
                             <p>A to-do list app inspired by the <a href='https://asana.com/resources/eisenhower-matrix' target='_blank' rel='noopener noreferrer'>Eisenhower Matrix</a></p>
+                        </div>
+                    )} 
+                    {projectOpen === 2 && (
+                        <div>
+                            <h3><a href='https://www.justinlucedev.com/minesweeper'>Minesweeper</a></h3>
+                        </div>                    
+                    )} 
+                    {projectOpen === 3 && (
+                        <div>
+                            <h3><a href='https://www.justinlucedev.com/chess'>Chess</a></h3>
                         </div>
                     )} 
                 </div>
